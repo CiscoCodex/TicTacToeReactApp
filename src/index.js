524db1271333f3703aedcd46764324e80898ac3b
CiscoCodex <61986003+CiscoCodex@users.noreply.github.com>
2020-03-18 13:21:37 -0700

    Working implementation
    Tic tac toe game that ends when a winner is declared

--- src/index.js
+++ src/index.js
@@ -2,23 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+// There are two React components
+// Board - renders 9 Squares
+// Game - renders the Board
+
+// onClick calls the parent onClick handler
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
+// the board 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  // has an array of  squares and keeps track of next player
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
   }
 
+  // handles game click, will set square to appropriate mark and not allow double clicks
+  //    or clicks when winner if found
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  // render a single square, has a value associated to it from 0 - 8
+  renderSquare(i) {
+    return (
+      <Square 
+       value={this.state.squares[i]} 
+       onClick={() => this.handleClick(i)}
+      />);
+  }
+
+  // render the board, check if there's a winner
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -43,6 +83,7 @@ class Board extends React.Component {
   }
 }
 
+// renders the game
 class Game extends React.Component {
   render() {
     return (
@@ -57,6 +98,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// helper function to check if there's a winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
